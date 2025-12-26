3da842e2e5071f5e18e0ec3730ff2d8ed8dea908
Rafael <rafael_guliev94@list.ru>
2025-12-26 16:48:10 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ insertCoin({
   skipLobby: false, // Включаем Лобби, чтобы игрок мог ввести имя и выбрать цвет!
   gameId: "bank-xmas-party",
   discord: false,
+  defaultStates: {},
+  streamMode: false,
 }).then(() => {
   // Правильный рендер для React 18
   const container = document.getElementById("root");
